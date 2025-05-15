61e47ecc4ca4a359379dcacadceb703e480ce736
L-ENA <lena.schmidt.0493@gmail.com>
2025-05-15 16:57:11 +0100

    Replaced R with python code and moved R code to branch

---
+++ pages/About.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.markdown(
+    '''# 🎲 :rainbow[RefRandomiser]: A tool to split and randomise data''')
+
+st.write("This app was created during the 2025 [EvidenceSynthesis Hackathon](https://www.eshackathon.org/) at UCL. All code is open-source and available on [Git-Hub](https://github.com/L-ENA/RefRandomiser/tree/master)")
+st.markdown(
+    '''## People''')
+
+st.write("[Lena Schmidt (UCL, Newcastle University)](https://www.researchgate.net/profile/Lena-Schmidt-6)")
+st.write("[Dr. Fiona Campbell (Newcastle University)](https://www.ncl.ac.uk/medical-sciences/people/profile/fionacampbell1.html)")
+st.write("[Dr. Matt Grainger (Norsk Institutt for Naturforskning)](https://www.nina.no/english/Contact/Employees/Employee-info?AnsattID=16028)")
+st.write("[Dr. Neal Haddaway (www.nealhaddaway.com)](www.nealhaddaway.com)")
+
+st.write('&nbsp;')  # empty line
+st.write("Please contact lena.schmidt@ucl.ac.uk with any queries or feedback about the tool.")
+st.write('&nbsp;')  # empty line
+st.write("If you found our tool helpful, you can refer to our [tool information sheet](https://osf.io/fkj46)")
+
+st.markdown(":gray[_Schmidt L., Grainger M.J., Haddaway N., Campbell F (2025, May 15). RefRandomiser: A tool to split and randomise data [Tool information Sheet]. Retrieved from osf.io/uanz4. DOI 10.17605/OSF.IO/UANZ4_]")
+
+
+
